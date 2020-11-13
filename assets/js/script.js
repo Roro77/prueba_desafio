f27387c1cb49d905b2eb461fc094d1db48ce62f9
Rodrigo <rodrigo.pineda.a07@gmail.com>
2020-11-12 23:15:39 -0300

    last works finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,16 @@
-$(function () {
+$(function(){
+    $('a').click(function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+
+            var move = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(move).offset().top
+            }, 800, function(){
+                window.location.hash = move;
+            });
+        }       
+    });
     $('[data-toggle="popover"]').popover();
-})
+});
